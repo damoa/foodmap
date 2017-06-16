b778655ffc73430eb5975b952a117860f176957a
Damoa <damoa@gmx.com>
2017-06-16 12:45:26 +0200

    Add filtering function.

--- javascript/map.js
+++ javascript/map.js
@@ -1,45 +1,81 @@
 function initMap() {
-  var render = function() {
+  var filterCategories;
+  var globalKeywords = [];
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: {lat: 52.511801, lng: 13.455590}
+  });
+  var markers = [];
+
+  var render = function(filterParams) {
+    if (markers.length > 0) {
+      for(var i=0; i < markers.length; i++) {
+        if (markers && markers[i]) {
+          markers[i].setMap(null);
+        }
+      }
+    }
+
+    var filterCategories = filterParams ? $.map(filterParams.match(/\S+\s*/g), function(val, index) { return val.trim(); }) : ['all'];
     var foodData;
     var geocoder = new google.maps.Geocoder();
-    var categories = [];
-    var markers = [];
     var infowindow = new google.maps.InfoWindow();
 
-    var uluru = {lat: 52.511801, lng: 13.455590};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: uluru
-    });
+    var addMarker = function(restaurant, i) {
+      if (markers[i]) {
+        markers[i].setMap(map);
+      } else {
+        geocoder.geocode( { 'address': restaurant.address }, function(results, status) {
+          if (results && results[0]) {
+            markers[i] = new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: map
+            });
 
-    var addMarker = function(restaurant) {
-      geocoder.geocode( { 'address': restaurant.address }, function(results, status) {
-        markers[i] = new google.maps.Marker({
-          position: results[0].geometry.location,
-          map: map
+
+            google.maps.event.addListener(markers[i], 'click', function() {
+              infowindow.setContent(restaurant.name);
+              infowindow.open(map, markers[i]);
+            });
+            markers.push(markers[i]);
+          } else {
+          }
         });
-
-        var marker = markers[i];
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(restaurant.name);
-          infowindow.open(map, marker);
-        });
-      });
+      }
     };
 
     $.getJSON('db/data.json', function(data) {
       foodData = data;
 
       for (i=0; i < data.restaurants.length; i++) {
-        if (categories.indexOf(foodData.restaurants[i].category) == -1) {
-          categories.push(foodData.restaurants[i].category);
+        var keywords = foodData.restaurants[i].keywords.match(/\S+\s*/g);
+        for (k=0; k < keywords.length; k++) {
+          var keyword = keywords[k].trim();
+          if (globalKeywords.indexOf(keyword) == -1) {
+            globalKeywords.push(keyword);
 
-          $('#controls ul').append("<li><input type='checkbox' id='js-asian'>" + foodData.restaurants[i].category + "</li>");
+            $('#controls ul').append("<li><input class='js-keyword-checkbox' type='checkbox' id='js-asian'>" + keyword + "</li>");
+          }
+          if (filterCategories.indexOf('all') == -1 && filterCategories.indexOf(keyword) > -1) {
+              addMarker(foodData.restaurants[i], i);
+          } else {
+            if (filterCategories.indexOf('all') > -1) {
+              console.log('add unfiltered');
+                addMarker(foodData.restaurants[i], i);
+            }
+          }
         }
-        addMarker(foodData.restaurants[i]);
       }
     });
+
+    $('.food-map').on('change', ':checkbox', function(){
+      var filterParams = '';
+      $('.js-keyword-checkbox:checked').each(function(index, element){
+        filterParams = filterParams.trim() + ' ' + $(this).parent().text();
+      });
+      render(filterParams);
+    });
   };
+
   render();
 }
